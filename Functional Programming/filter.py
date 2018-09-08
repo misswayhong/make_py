ebbbd197b4194f2884a99852ee3e53205f77c20e
water <shanpaie@126.com>
2018-09-08 19:26:53 +0800

    ceshiyixia

--- Functional Programming/filter.py
+++ Functional Programming/filter.py
@@ -2,3 +2,14 @@
 # 不同的是，filter把传入的函数一次作用于每个元素，然后根据
 # 返回值是True还是False来决定是否保留该元素（返回为惰性数列）
 
+# 判断回数，并用filter()筛选出来
+
+
+def fn(n):
+    return str(n) == str(n)[::-1]
+
+
+outer = list(filter(fn, range(1, 100)))
+
+print(outer)
+
